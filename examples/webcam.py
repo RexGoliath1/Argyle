95ab8f53aa0b3250898d464257cfae2d5e2cbf7e
RexGoliath1 <sgoncia@gmail.com>
2019-04-24 22:13:07 -0500

    Directory Stuff

--- examples/webcam.py
+++ examples/webcam.py
@@ -1,9 +1,11 @@
 import cv2
+import os
 
 #Initialize camera
 cam = cv2.VideoCapture(0);
 ret, image = cam.read();
 
 if ret:
+  os.system('mkdir ./pics/')
   cv2.imwrite("./pics/ex1.jpg", image)
 cam.release()
