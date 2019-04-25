88d2d769b86a3727112b6782d77c49ffa45bf774
RexGoliath1 <sgoncia@gmail.com>
2019-04-24 22:25:57 -0500

    Webcam2

--- examples/webcam.py
+++ examples/webcam.py
@@ -2,10 +2,19 @@ import cv2
 import os
 
 #Initialize camera
-cam = cv2.VideoCapture(0);
-ret, image = cam.read();
+cam1 = cv2.VideoCapture(0);
+cam2 = cv2.VideoCapture(0);
 
-if ret:
+ret1, image1 = cam1.read();
+ret2, image2 = cam2.read();
+
+if ret1:
   os.system('mkdir ./pics/')
-  cv2.imwrite("./pics/ex1.jpg", image)
-cam.release()
+  cv2.imwrite("./pics/ex1.jpg", image1)
+
+if ret2:
+  os.system('mkdir ./pics/')
+  cv2.imwrite("./pics/ex2.jpg", image2)
+
+cam1.release()
+cam2.release()
